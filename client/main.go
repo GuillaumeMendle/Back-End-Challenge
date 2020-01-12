3375a95abd38327c709e05cfd6a419b0a0ec3dc7
GuillaumeMendle <46578572+GuillaumeMendle@users.noreply.github.com>
2020-01-12 16:31:08 +0000

    Add files via upload

---
+++ client/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	//"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+	//"github.com/gorilla/mux"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var mySigningKey = []byte("password1")
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GenerateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, validToken)
+}
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["client"] = "Client A"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("Something Went Wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+func main() {
+	handleRequests()
+}
